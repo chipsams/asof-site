f41e1f10306a63f6825765122d317ccf83e068f9
chipsams <chipsamsPROGRAMMING@gmail.com>
2026-04-08 22:37:45 +0100

    update density function thingy

---
+++ experiments/density-functions/parser.js
@@ -0,0 +1,196 @@
+let depth = 0
+let toks = []
+
+const makeUUID = ()=>crypto.randomUUID()
+
+const simpleWrap = (fn,extraprops,...argNames)=>{
+    let attr = {}
+    if(typeof extraprops == "string") extraprops = {type:extraprops}
+    if(extraprops._delayedEval) attr.delayedEval = delete extraprops._delayedEval
+
+    return {_fn:fn,type:extraprops.type,_mn:(ctx,...args)=>{
+        let dat = Object.assign({},extraprops)
+        for(let l=0;l<argNames.length;l++){
+            dat[argNames[l]+((args[l]===undefined)?"_":"")] = args[l]??"NOT PROVIDED"
+        }
+        return dat
+    },attr}
+}
+const manyParam = (fn,extraprops,arg1,arg2)=>{
+    return {_fn:fn,_mn:(ctx,...args)=>{
+        if(typeof extraprops == "string") extraprops = {type:extraprops}
+        let dat = Object.assign({},extraprops)
+        for(let l=0;l<args.length;l++){
+            dat[argNames[l]] = args[l]
+        }
+        return dat
+    }}
+}
+
+let constants = {
+    x: {type:"moredfs:x"},
+    y: {type:"moredfs:y"},
+    z: {type:"moredfs:z"}
+}
+
+let cache = {}
+
+let methods = {
+    cache2d:simpleWrap((self,ctx,a)=>{
+        cache[self.id]??=[]
+        cache[self.id][ctx.x+","+ctx.z]??=a()
+        return cache[self.id][ctx.x+","+ctx.z]
+    },{_delayedEval:true,type:"minecraft:cache_2d"},"argument"),
+    perlin:simpleWrap((self,_,x,y,sX,sY)=>noise.perlin2(x/sX,y/sY),
+        "noise","x","z","scale_x","scale_z"),
+    distance:simpleWrap((self,_,a,b)=>Math.sqrt(a.map((v,i)=>v-(b?b[i]:0)).reduce((a,b)=>a+b*b,0)),
+        {type:"moredfs:distance",distance_metric:{type:"euclidian"}},"points1","points2"),
+    clamp:simpleWrap((self,_,v,l,u)=>Math.max(Math.min(v,u),l),"moredfs:clamp","argument","min","max"),
+    clamp01:simpleWrap((self,_,v)=>Math.max(Math.min(v,1),0),{type:"moredfs:clamp",min:0,max:1},"argument"),
+    clamp11:simpleWrap((self,_,v)=>Math.max(Math.min(v,1),-1),{type:"moredfs:clamp",min:-1,max:1},"argument"),
+    floor:simpleWrap((self,_,v)=>Math.floor(v),"moredfs:floor","argument"),
+    ceil:simpleWrap((self,_,v)=>Math.ceil(v),"moredfs:ceil","argument"),
+    round:simpleWrap((self,_,v)=>Math.round(v),"moredfs:round","argument"),
+    sin:simpleWrap((self,_,v)=>Math.sin(v),"moredfs:sin","argument"),
+    cos:simpleWrap((self,_,v)=>Math.cos(v),"moredfs:cos","argument"),
+    abs:simpleWrap((self,_,v)=>Math.abs(v),"minecraft:abs","argument"),
+    sign:simpleWrap((self,_,v)=>Math.sign(v),"moredfs:signum","argument")
+}
+
+function binaryFormat(type,p1="argument1",p2="argument2"){
+    return (_,a,b)=>({type:type,[p1]:a,[p2]:b})
+}
+
+const simple = (ctx,a,b)=>({type:ctx.obj.type,"paramter1":a,"paramter2":b})
+
+let binaryOperators = {
+    "^": {_mn:binaryFormat("moredfs:power","base","exponent"),_fn:(self,_,a,b)=>a**b,prec:0},
+    "*": {_mn:binaryFormat("minecraft:mul"),_fn:(self,_,a,b)=>a*b,prec:1},
+    "/": {_mn:binaryFormat("moredfs:div","numerator","denominator"),_fn:(self,_,a,b)=>a/b,prec:1},
+    "%": {_mn:binaryFormat("moredfs:mod","numerator","denominator"),_fn:(self,_,a,b)=>(a%b+b)%b,prec:1},
+    "+": {_mn:binaryFormat("minecraft:add"),_fn:(self,_,a,b)=>a+b,prec:2},
+    "-": {_mn:binaryFormat("moredfs:subtract"),_fn:(self,_,a,b)=>a-b,prec:2},
+}
+
+function debugify(...args){
+    //console.log("=|".repeat(depth),...args)
+    //console.log("=|".repeat(depth),toks.join(" "))
+}
+
+function expectAndShift(toks,symbol){
+    if(toks.shift() !== symbol) throw new Error(`expected ${symbol}!`)
+}
+
+function parseValList(toks){
+    depth++
+    let values = [parseExpr(toks)]
+    while(toks.at(0)==","){
+        debugify("comma")
+        expectAndShift(toks,",")
+        values.push(parseExpr(toks))
+    }
+    depth--
+    return values
+}
+
+/** @param {string[]} toks */
+function parseVal(toks){
+
+    let data = {};
+    
+    if(toks.at(0) == "("){
+        debugify("parsing bracketed")
+        expectAndShift(toks,"(")
+        data = parseExpr(toks)
+        if(toks.shift()!==")") throw new Error("unclosed (!! (bracketed expression)")
+        debugify("done parsing bracketed")
+        return data;
+    }
+
+    if(toks.at(0) == "["){
+        debugify("parsing array")
+        expectAndShift(toks,"[")
+        data = parseValList(toks)
+        if(toks.shift()!=="]") throw new Error("unclosed [!! (array)")
+        debugify("done parsing array")
+        return data;
+    }
+    
+    if(/^([0-9])+(.([0-9])+)?$/.test(toks.at(0)??"")){
+        return parseFloat(toks.shift())
+    }
+
+    if(/^[a-z][a-z_0-9]*(?::[a-z_0-9]+)?$/.test(toks.at(0)??"")){
+        let tok = toks.shift()
+        if(toks.at(0)=="("){
+            debugify("parsing function",tok)
+            expectAndShift(toks,"(")
+            let args = parseValList(toks)
+            if(toks.shift()!==")") throw new Error("unclosed )!! (function parameters)")
+                debugify("done parsing function",tok)
+            if(typeof methods[tok] == "function") throw new Error("used old function "+tok)
+            return {type:"function:"+tok,args:args,_fn:methods[tok]._fn,_mn:methods[tok]._mn,id:makeUUID(),attr:methods[tok].attr}
+        }else{
+            return {type:"constant:"+tok,_autogen:"yes",_fn:(self,ctx)=>ctx[tok],_mn:()=>{
+                return constants[tok]
+            },id:makeUUID()}
+        }
+    }
+
+    throw new Error("expected a value!")
+}
+
+function parseMonad(toks){
+    if(toks.at(0)=="-"){
+        toks.shift()
+        return {
+            type:"moredfs:negate",
+            args:[parseMonad(toks)],
+            _fn:(self,_,v)=>Array.isArray(v)?v.map(v=>-v):-v,
+            _mn:(_,a)=>({type:"moredfs:negate",argument:a}),
+            id:makeUUID()
+        };
+    }
+    return parseVal(toks)
+}
+
+/** @param {string[]} toks */
+function parseExpr(toks,prec = 2){
+    let value = prec>0?parseExpr(toks,prec-1):parseMonad(toks)
+    while(binaryOperators[toks.at(0)]?.prec == prec){
+        let op = toks.shift()
+        debugify("parsing op",op)
+        let value2 = prec>0?parseExpr(toks,prec-1):parseMonad(toks)
+        value = {type:"operator:"+op,args:[value,value2],_fn:binaryOperators[op]._fn,_mn:binaryOperators[op]._mn,id:makeUUID()}
+    }
+    return value
+}
+
+const numberRegex = /([0-9]+(\.[0-9]+)?)/
+const symbolRegex = /[\(\)\[\],\<\>\+\-\*\%\/\^]/
+const identifierRegex = /[a-z][a-z_0-9]*(?::[a-z_0-9]+)?/
+let codeRegex = new RegExp([
+    numberRegex.source,
+    symbolRegex.source,
+    identifierRegex.source
+].join("|"),"g")
+
+function parseCode(code){
+    cache = {}
+    let toks = [...code.matchAll(codeRegex)].map(([v])=>v)
+    return parseExpr(toks)
+}
+
+function evaluate(ctx,parsed){
+    if(parsed == undefined) return undefined;
+    if(typeof parsed == "number") return parsed;
+    if(Array.isArray(parsed)) return parsed.map(v=>evaluate(ctx,v));
+    if(parsed._fn){
+        let args = parsed.attr?.delayedEval
+            ?parsed.args?.map?.(v=>()=>evaluate(ctx,v)) ?? []
+            :parsed.args?.map?.(v=>evaluate(ctx,v)) ?? []
+        let out = parsed._fn(parsed,ctx,...args)
+        if(ctx.logging)console.log(parsed.type,args,"->",out)
+        return out
+    }
+}
--- experiments/density-functions/script.js
+++ experiments/density-functions/script.js
@@ -1,163 +1,4 @@
-let depth = 0
-let toks = []
 
-const simpleWrap = (fn,extraprops,...argNames)=>{
-    return {_fn:fn,_mn:(ctx,...args)=>{
-        if(typeof extraprops == "string") extraprops = {type:extraprops}
-        let dat = Object.assign({},extraprops)
-        for(let l=0;l<args.length;l++){
-            dat[argNames[l]] = args[l]
-        }
-        return dat
-    }}
-}
-
-let constants = {
-    x: {type:"moredfs:x"},
-    y: {type:"moredfs:y"},
-    z: {type:"moredfs:z"}
-}
-
-let methods = {
-    perlin:simpleWrap((_,x,y,sX,sY)=>noise.perlin2(x/sX,y/sY),
-        "noise","x","z","scale_x","scale_z"),
-    distance:simpleWrap((_,a,b)=>Math.sqrt(a.map((v,i)=>v-b[i]).reduce((a,b)=>a+b*b,0)),
-        {type:"moredfs:distance",distance_metric:{type:"euclidian"}},"points1","points2"),
-    clamp:(_,v,u,l)=>Math.max(Math.min(v,u),l),
-    clamp01:(_,v)=>Math.max(Math.min(v,1),0),
-    clamp11:(_,v)=>Math.max(Math.min(v,1),-1),
-    floor:simpleWrap((_,v)=>Math.floor(v),"moredfs:floor","argument"),
-    ceil:simpleWrap((_,v)=>Math.ceil(v),"moredfs:ceil","argument"),
-    round:simpleWrap((_,v)=>Math.round(v),"moredfs:round","argument"),
-    sin:simpleWrap((_,v)=>Math.sin(v),"moredfs:sin","argument"),
-    cos:simpleWrap((_,v)=>Math.cos(v),"moredfs:cos","argument"),
-    abs:simpleWrap((_,v)=>Math.abs(v),"minecraft:abs","argument"),
-    ceil:(_,v)=>Math.ceil(v),
-    sign:(_,v)=>Math.sign(v),
-    lerp:(_,a,b,t)=>(1-t)*a+b*t,
-}
-
-function binaryFormat(type,p1="argument1",p2="argument2"){
-    return (_,a,b)=>({type:type,[p1]:a,[p2]:b})
-}
-
-const simple = (ctx,a,b)=>({type:ctx.obj.type,"paramter1":a,"paramter2":b})
-
-let binaryOperators = {
-    "<": {_mn:()=>({"not":"implemented :("}),_fn:(_,a,b)=>a>b?1:0,prec:0},
-    ">": {_mn:()=>({"not":"implemented :("}),_fn:(_,a,b)=>a<b?1:0,prec:0},
-    "*": {_mn:binaryFormat("minecraft:mul"),_fn:(_,a,b)=>a*b,prec:1},
-    "/": {_mn:binaryFormat("moredfs:div","numerator","denominator"),_fn:(_,a,b)=>a/b,prec:1},
-    "%": {_mn:binaryFormat("moredfs:mod","numerator","denominator"),_fn:(_,a,b)=>(a%b+b)%b,prec:1},
-    "+": {_mn:binaryFormat("minecraft:add"),_fn:(_,a,b)=>a+b,prec:2},
-    "-": {_mn:binaryFormat("moredfs:subtract"),_fn:(_,a,b)=>a-b,prec:2},
-}
-
-function debugify(...args){
-    //return;
-    console.log("=|".repeat(depth),...args)
-    console.log("=|".repeat(depth),toks.join(" "))
-}
-
-function expectAndShift(toks,symbol){
-    if(toks.shift() !== symbol) throw new Error(`expected ${symbol}!`)
-}
-
-function parseValList(toks){
-    depth++
-    let values = [parseExpr(toks)]
-    while(toks.at(0)==","){
-        debugify("comma")
-        expectAndShift(toks,",")
-        values.push(parseExpr(toks))
-    }
-    depth--
-    return values
-}
-
-/** @param {string[]} toks */
-function parseVal(toks){
-
-    let data = {};
-    
-    if(toks.at(0) == "("){
-        debugify("parsing bracketed")
-        expectAndShift(toks,"(")
-        data = parseExpr(toks)
-        if(toks.shift()!==")") throw new Error("unclosed (!! (bracketed expression)")
-        debugify("done parsing bracketed")
-        return data;
-    }
-
-    if(toks.at(0) == "["){
-        debugify("parsing array")
-        expectAndShift(toks,"[")
-        data = parseValList(toks)
-        if(toks.shift()!=="]") throw new Error("unclosed [!! (array)")
-        debugify("done parsing array")
-        return data;
-    }
-    
-    if(/^([0-9])+(.([0-9])+)?$/.test(toks.at(0)??"")){
-        return parseFloat(toks.shift())
-    }
-
-    if(/^[a-z][a-z_0-9]*(?::[a-z_0-9]+)?$/.test(toks.at(0)??"")){
-        let tok = toks.shift()
-        if(toks.at(0)=="("){
-            debugify("parsing function",tok)
-            expectAndShift(toks,"(")
-            let args = parseValList(toks)
-            if(toks.shift()!==")") throw new Error("unclosed )!! (function parameters)")
-                debugify("done parsing function",tok)
-            if(typeof methods[tok] == "function") throw new Error("used old function "+tok)
-            return {type:"function:"+tok,args:args,_fn:methods[tok]._fn,_mn:methods[tok]._mn}
-        }else{
-            return {type:"constant:"+tok,_autogen:"yes",_fn:(ctx)=>ctx[tok],_mn:()=>{
-                console.log(tok,constants[tok])
-                return constants[tok]
-            }}
-        }
-    }
-
-    throw new Error("expected a value!")
-}
-
-function parseMonad(toks){
-    if(toks.at(0)=="-"){
-        toks.shift()
-        return {
-            type:"moredfs:negate",
-            args:[parseMonad(toks)],
-            _fn:(_,v)=>Array.isArray(v)?v.map(v=>-v):-v,
-            _mn:(_,a)=>({type:"moredfs:negate",argument:a})}
-    }
-    return parseVal(toks)
-}
-
-/** @param {string[]} toks */
-function parseExpr(toks,prec = 2){
-    let value = prec>0?parseExpr(toks,prec-1):parseMonad(toks)
-    while(binaryOperators[toks.at(0)]?.prec == prec){
-        let op = toks.shift()
-        debugify("parsing op",op)
-        let value2 = prec>0?parseExpr(toks,prec-1):parseMonad(toks)
-        value = {type:"operator:"+op,args:[value,value2],_fn:binaryOperators[op]._fn,_mn:binaryOperators[op]._mn}
-    }
-    return value
-}
-
-function evaluate(ctx,parsed){
-    if(parsed == undefined) return undefined;
-    if(typeof parsed == "number") return parsed;
-    if(Array.isArray(parsed)) return parsed.map(v=>evaluate(ctx,v));
-    if(parsed._fn){
-        let args = parsed.args?.map?.(v=>evaluate(ctx,v)) ?? []
-        let out = parsed._fn(ctx,...args)
-        if(ctx.logging)console.log(parsed.type,args,"->",out)
-        return out
-    }
-}
 
 function toOutput(object){
     if(typeof object == "number") return object;
@@ -176,14 +17,30 @@ function toOutput(object){
     return dat
 }
 
+function clamp(v,l,u){
+    return Math.min(Math.max(v,l),u)
+}
+
+function binarySearch(fn,min,max,guess=(min+max)/2){
+    let bounds = [min,max]
+    for(let l=0;l<11;l++){
+        let oldGuess = guess
+        let key = fn(guess)>0?0:1;
+        [bounds[key],guess] = [guess,guess/2+bounds[1-key]/2]
+        
+        if(Math.abs(oldGuess - guess)<0.25) break
+    }
+    return guess
+}
+
 function draw(fn){
     let img = new ImageData(128,128)
     for(let z=0;z<128;z++){
         for(let x=0;x<128;x++){
-            let v = fn(x,z)
-            img.data[z*4*128+x*4] = Math.max(0,v*255);
-            img.data[z*4*128+x*4+1] = Math.max(0,-v*255);
-            img.data[z*4*128+x*4+2] = 255;
+            let [r,g,b] = fn(x,z)
+            img.data[z*4*128+x*4] = r;
+            img.data[z*4*128+x*4+1] = g;
+            img.data[z*4*128+x*4+2] = b;
             img.data[z*4*128+x*4+3] = 255;
         }
     }
@@ -192,31 +49,98 @@ function draw(fn){
     let ctx = canvas.getContext("2d");
     ctx.putImageData(img,0,0)
 }
+function preCompute(fn){
+    let outputs=[]
+    for(let z=0;z<128;z++){
+        outputs[z]=[]
+        for(let x=0;x<128;x++){
+            outputs[z][x]=fn(x,z)
+        }
+    }
+    return outputs
+}
 
-function rerun(code){
-    toks = [...code.matchAll(/[\(\)\[\],\<\>\+\-\*\%\/]|([0-9]+(\.[0-9]+)?)|[a-z][a-z_0-9]*(?::[a-z_0-9]+)?/g)].map(([v])=>v)
-    let parsed;
+let codeArea = document.getElementById("code")
+function rerun(){
+    let code = codeArea.value
 
     try{
-        parsed = parseExpr(toks)
-        console.log(parsed)
+        let parsed = parseCode(code)
     
         let out = document.getElementById("output")
         out.value = JSON.stringify(toOutput(parsed),null,2)
             .replaceAll(/\[\s*\d+(\s*,\s*\d+)*\s*\]/g,(v)=>v.replaceAll(/\s/g,""))
             .replaceAll(/\{\s*"type"\s*:\s*"(\w+(?::\w+)?)"\s*(})?/g,'{"type":"$1"$2')
-        draw((x,z)=>evaluate({x,z,y:64},parsed))
+        
+        if(settings["topology-view"]){
+            let heights = preCompute((x,z)=>binarySearch((guess)=>evaluate({x:x+settings["offset-x"],z:z+settings["offset-z"],y:guess},parsed),-64,320))
+            draw((x,z)=>{
+                let height = heights[z][x]
+                let inWater = height<settings["y-pos"]
+
+                let darken = (heights[z-1]?.[x]??height)+(heights[z]?.[x-1]??height)-height*2
+                if(darken<0) darken = 0
+                let brighten = height*2-(heights[z-1]?.[x]??height)-(heights[z]?.[x-1]??height)
+                if((brighten<0) || inWater) brighten = 0
+
+                return [clamp((height+64)/384*255,0,255),0,inWater?128:0].map(v=>v+brighten*8-darken*8)
+            })
+        }else{
+            draw((x,z)=>{
+                let v = evaluate({x:x+settings["offset-x"],z:z+settings["offset-z"],y:settings["y-pos"]},parsed)
+                return [Math.max(0,v*255),Math.max(0,-v*255),255]
+            })
+        }
     }catch(e){
         let out = document.getElementById("output")
         out.value = e
-        draw(()=>Math.random()*2-1)
+        draw((x,z)=>{
+            return [clamp(x*2+Math.random()*20,0,255),z*2,255]
+        })
         return;
     }
 }
 
-let codeArea = document.getElementById("code")
+let settings = {}
+function settingschanged(){
+    rerun()
+}
+
+let settingsTab = document.getElementById("settings")
+let settingInputsMap = {}
+let settingInputs = [...settingsTab.querySelectorAll("[name]")]
+let settingResets = [...settingsTab.querySelectorAll("[reset]")]
+
+function parseSetting(e){
+    switch(e.getAttribute("type")){
+        case "range": return parseFloat(e.value)
+        case "checkbox": return e.checked
+        default: return e.value
+    }
+}
+
+for(let setting of settingInputs){
+    let name = setting.getAttribute("name")
+    settings[name] = parseSetting(setting)
+    settingInputsMap[name] = setting
+    setting.addEventListener("input",()=>{
+        settings[name] = parseSetting(setting)
+        settingschanged()
+    })
+    settingschanged()
+}
+for(let reset of settingResets){
+    let name = reset.getAttribute("reset")
+    reset.addEventListener("click",event=>{
+        settingInputsMap[name].value = reset.getAttribute("value")
+        settings[name] = parseSetting(settingInputsMap[name])
+        settingschanged()
+
+    })
+
+}
 
 codeArea.addEventListener("input",e=>{
-    rerun(codeArea.value)
+    rerun()
 })
-rerun(codeArea.value)
+rerun()
